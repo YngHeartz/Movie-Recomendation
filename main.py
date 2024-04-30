3df47f1766614998ba3a9bbf8590c35dbb17c3e0
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-04-30 16:12:11 -0400

    Comments

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
-
+# api connection
 API_KEY = 'df7cb704b97a64416b2429bd03b5171e'
-
+# Movie Input Title Information
 movie_search = input("What movie are you looking for?")
-
+# This will search the api and take the parameter given by the user from the movie_search input variable
 movie_information = requests.get(f"https://api.themoviedb.org/3/search/{movie_search}?")
-
+# Output the Movie Information in JSON format and print it out to the User
 print(f"You are looking for {movie_search}, The basic information about this movie is: {movie_information}")
-
+# retrieves the movie from the api and fills the title from the movie_search input variable
 url = (f"https://api.themoviedb.org/3/search/movie?query={movie_search}&api_key=df7cb704b97a64416b2429bd03b5171e")
 
 response = requests.get(url)
